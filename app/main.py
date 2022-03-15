abf3e4fe9045c20da4186e4ef30bdca27901e9ef
xiezhang <xiezhang@redhat.com>
2022-03-15 16:47:26 -0400

    Integrate with service binding on Openshift

--- app/main.py
+++ app/main.py
@@ -1,26 +1,40 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from pyservicebinding import binding
+
+sb = binding.ServiceBinding()
+pg_list = sb.bindings("postgresql")
+pg = pg_list[0]
+username = pg["username"]
+password = pg["password"]
+database = pg["database"]
+host = pg["host"]
+port = pg["port"]
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://'+username+':'+password+'@'+host+':'+port+'/'+database
 app.config['SECRET_KEY'] = "random string"
 
 db = SQLAlchemy(app)
 
 
 class students(db.Model):
+    __tablename__ = 'students'
+
     id = db.Column('student_id', db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     city = db.Column(db.String(50))
     addr = db.Column(db.String(200))
     pin = db.Column(db.String(10))
 
+    def __init__(self, name, city, addr, pin):
+        self.name = name
+        self.city = city
+        self.addr = addr
+        self.pin = pin
 
-def __init__(self, name, city, addr, pin):
-    self.name = name
-    self.city = city
-    self.addr = addr
-    self.pin = pin
+    def __repr__(self):
+        return f"<Student {self.name}>"
 
 
 @app.route('/')
@@ -44,6 +58,6 @@ def new():
     return render_template('new.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
